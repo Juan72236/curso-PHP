b8bae70d5234356a035d279390d4a4f8a270a2a5
JuanDeveloper <juanpablov018@gmail.com>
2023-01-13 18:13:22 -0500

    20230113_1

--- Formularios/index.php
+++ Formularios/index.php
@@ -1,31 +1,60 @@
+<?php
+
+$errores = '';
+
+if(isset($_POST['submit'])){
+    $nombre = $_POST['nombre'];
+    $correo = $_POST['correo'];
+
+    if(!empty($nombre)){
+        /* $nombre = trim($nombre);
+        $nombre = htmlspecialchars($nombre);
+        $nombre = stripslashes($nombre); */
+
+        /* $nombre = filter_var($nombre, FILTER_SANITIZE_STRING); limpia los datos ingresados en un campo de texto, removiendo cualquuier etiqueta o caracter que pueda alterar el código o la base de datos. */
+
+        echo 'Su nombre es: ' . $nombre . '<br />';
+    }else{
+        $errores .= 'Por favor agrega un nombre' . '<br />';
+    }
+
+    if(!empty($correo)){
+        filter_var($correo, FILTER_SANITIZE_EMAIL); //Elimina caracteres basura del correo, protegiendo el codigo. 
+        
+        if(!filter_var($correo, FILTER_VALIDATE_EMAIL)){
+            $errores = 'Por favor ingresa un correo valido <br />';
+        }else{
+            echo 'Tu correo es: ' . $correo;
+        }
+        
+        echo 'Su correo es: ' . $correo . '<br />'; 
+    }else{
+        $errores .= 'Por favor agrega un correo';
+    }
+
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Estructura php de un Formulario</title>
+    <title>Formulario</title>
 </head>
 <body>
-    <form action="recibe.php" method="get" name="">
-        <input type="text" placeholder="Nombre:" name="nombre">
-        <br>
-        <label for="hombre">Hombre</label>
-        <input type="radio" name="sexo" value="hombre">
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+        <input type="text" name="nombre" id="nombre" placeholder="Nombre">
+        <input type="email" name="correo" id="correo" placeholder="correo">
 
-        <label for="Mujer">Mujer</label>
-        <input type="radio" name="sexo" value="Mujer">
-
-        <select name="year" id="">
-            <option value="2000">2000</option>
-            <option value="2001">2001</option>
-            <option value="2002">2002</option>
-        </select>
-
-        <label for="terminos">Terminos y Condiciones</label>
-        <input type="checkbox" name="terminos" id="terminos" value="ok">
-
-        <input type="submit" value="enviar">
+        <?php if(!empty($errores)) : ?>
+            <div class="error"><?php echo $errores ?></div>
+        <?php endif; ?>
+        
+        <input type="submit" value="Enviar" name="submit">
     </form>
 </body>
 </html>
